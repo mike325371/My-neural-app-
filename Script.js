e593d48c13c99c40c6f95fe8faa734281410b5af
mike325371 <aethelvault@gmail.com>
2025-05-25 03:09:47 -0500

    Update Script.js

--- Script.js
+++ Script.js
@@ -1,3 +1,14 @@
+// Group H 1of4: trainModel() function aaaaaaa
 function trainModel() {
-    alert("Training module started...");
+  const bar = document.getElementById('progressBar');
+  let width = 0;
+  const interval = setInterval(() => {
+    if (width >= 100) {
+      clearInterval(interval);
+    } else {
+      width++;
+      bar.style.width = width + '%';
+    }
+  }, 50); // Adjust speed as needed
 }
+// End Group H 1of4
